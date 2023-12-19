f03b11c96729e6318bc0a5f63d23644c928dfbe5
Proskurova <proskurovalyuda@gmail.com>
2023-12-19 13:42:30 +0300

    Сделала адаптацию карточки товара, корзины, всех popup(таблица размеров, задать вопрос) под разные устройства.

--- lovelev_site/clothes/templatetags/clothes_tags.py
+++ lovelev_site/clothes/templatetags/clothes_tags.py
@@ -2,24 +2,25 @@ from django import template
 from clothes.forms import QuestionUserForm
 
 from clothes.models import MenuItem
+from cart.cart import Cart
 register = template.Library()
 
 
 @register.inclusion_tag('clothes/menu.html', takes_context=True)
 def show_menu(context):
     menu_items = MenuItem.objects.filter(level=1)
+    # cart = Cart()
     return {
         "menu_items": menu_items,
+        # "card": cart,
     }
 
 
 @register.inclusion_tag('clothes/footer.html', takes_context=True)
 def show_footer(context):
-    footer_items = MenuItem.objects.filter(level=1)
     question_user_form = QuestionUserForm()
     popup = 'popup_question'
     return {
-        "footer_items": footer_items,
         "question_user_form": question_user_form,
         "popup": popup
     }
