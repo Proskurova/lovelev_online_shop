265efc8c18181b6c6c99a6ef77742b88ec4bd424
Proskurova <proskurovalyuda@gmail.com>
2023-12-22 00:40:57 +0300

    Загрузила товар и нашла ошибку в сохранении корзины в сессии, исправила файл cart.py метод __iter__

--- lovelev_site/cart/views.py
+++ lovelev_site/cart/views.py
@@ -31,7 +31,6 @@ def cart_remove(request, product_id, size):
 def cart_detail(request):
     cart = Cart(request)
     cart_form = OrderCreateForm
-    print(f"Проверка cart:---- {cart}/n{cart_form}")
     return render(request, 'cart/detail.html', {'cart': cart, 'cart_form': cart_form, 'title': 'Корзина покупок'})
 
 
